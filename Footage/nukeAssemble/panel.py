a7760b1caabe24f670b49dae8277efcf43918dc4
Drew Loveridge <mayaenite@hotmail.com>
2023-01-29 18:04:31 -0500

    Changed Name Of Menu Item and pannle added autoplace

--- Footage/nukeAssemble/panel.py
+++ Footage/nukeAssemble/panel.py
@@ -6,11 +6,11 @@ import nuke_crop_assemble
 #-------------------------------------------------------------------
 # Menu_Item
 ## [menu_item]
-##   label: Nuke Crop Assemble
+##   label: Rendered Tiles Assembly
 ##   tooltip: Tool Tip Needed
 def cropAssmble():
     ### this works or does it, damn it###
-    p = nuke.Panel('Nuke Crop Assembler')
+    p = nuke.Panel('Rendered Tiles Assembly')
     p.addFilenameSearch('path_to_tiles', '')
     if p.show():
         print p.value('path_to_tiles')
